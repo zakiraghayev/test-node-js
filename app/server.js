d762d019ed237235470ea2420ce49fd673e24b7e
Zakir Aghayev <agayevzakir239@gmail.com>
2024-04-21 01:57:21 +0400

    cors

--- app/server.js
+++ app/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const db = require('./database');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
+app.use(cors()); 
 
 // API endpoints
 // Get all items
